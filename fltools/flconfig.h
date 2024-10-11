dae58ea0de6c8b99c59f159b84fd115664a23ae1
Thomas Fänge <thomas.fange@sony.com>
2024-10-11 14:54:50 +0200

    Updated flash tool to show a map over the flash Updated config file with flash area mapping

--- fltools/flconfig.h
+++ fltools/flconfig.h
@@ -1,27 +1,69 @@
 #ifndef __FLCONFIG_H__
 #define __FLCONFIG_H__
 
+#if defined(PIMORONI_PICOLIPO_16MB)
+#define FLASH_SIZE  ((16*1024*1024)-1)
+#elif defined(PIMORONI_TINY2040_8MB)
+#define FLASH_SIZE  ((8*1024*1024)-1)
+#elif defined(RASPBERRYPI_PICO2)
+#define FLASH_SIZE  ((4*1024*1024)-1)
+#else
+#ifdef NO_EXTERNAL
+// Assume a simple Pico2 board
+#define FLASH_SIZE  ((4*1024*1024)-1)
+#else
+#error("No or bad board defined!")
+#endif
+#endif
+
 #define PICO_FLASH_START 0x10000000
 
-#define PG_SIZE     (4*1024)
-#define FLASH_START (128*PG_SIZE)  // Offset to flash area (512kB)
+//#define RAM_SIZE    0x1000
+#define PAGE_SIZE   0x1000
+#define ROM_SIZE    0x1000
+#define PAGE_MASK   0x0FFF
+#define ADDR_MASK   0xFFFF
+#define NR_PAGES    0x10
+#define NR_BANKS    4
+//#define LAST_PAGE   (NR_PAGES - 1)
 
-#define CONF_PAGE   ((7*PG_SIZE)+FLASH_START+PICO_FLASH_START)
+#define PG_SIZE           (4*1024)
+#define PORT_SIZE         (2*PG_SIZE)
+#define FLASH_START       (128*PG_SIZE)  // Offset to flash area (512kB)
 
-// Offset to FAT
-#define FAT_START   (8*PG_SIZE)+FLASH_START+PICO_FLASH_START
-#define FAT_SIZE    (4)           // Size of FAT (pages)
+#define FL_ADDR(x)        (((x)*PG_SIZE)+FLASH_START+PICO_FLASH_START)
 
-#define FFS_START   FAT_START+(FAT_SIZE*PG_SIZE)
-#define FFS_SIZE    (16*4*2)
+// Pages defines for XMemory strage
+#define XMEM_OFFS         0
+#define XMEM_PAGE(x)      FL_ADDR(x+XMEM_OFFS)
+#define XMEM_SIZE         7
 
-#define FS_START    FFS_START+(FFS_SIZE*PG_SIZE)
+// Page to hold the saved configuration and setup
+#define CONF_OFFS         (XMEM_OFFS+XMEM_SIZE)
+#define CONF_PAGE(x)      FL_ADDR(x+CONF_OFFS)
+#define CONF_SIZE         1
+
+// Offset to the FFS FAT
+#define FAT_OFFS          (CONF_OFFS+CONF_SIZE)
+#define FAT_PAGE(x)       FL_ADDR(x+FAT_OFFS)
+#define FAT_START         FL_ADDR(FAT_OFFS)
+#define FAT_SIZE          4           // Size of FAT (pages)
+
+// Offset to the ROMMAP
+#define ROMMAP_OFFS       (FAT_OFFS+FAT_SIZE)
+#define ROMMAP_START      FL_ADDR(ROMMAP_OFFS)
+#define ROMMAP_PAGE(p,b)  FL_ADDR(ROMMAP_OFFS+(p*NR_BANKS+b)*2)
+//#define ROMMAP_PAGE(p,b)  (ROMMAP_START+((p*NR_BANKS+b)*PORT_SIZE))
+#define ROMMAP_SIZE       (16*4*2) // 16 pages x 4 banks x 2*4kb
+
+#define FS_OFFS           (ROMMAP_OFFS+ROMMAP_SIZE)
+#define FS_START          FL_ADDR(FS_OFFS)
+#define FS_PAGE(x)        FL_ADDR(x+FS_OFFS)
 
 #ifndef NR_BANKS
-#define FFS_PAGE(p,b) (FFS_START+((p*4+b)*2*PG_SIZE))
-#else
-#define FFS_PAGE(p,b) (FFS_START+((p*NR_BANKS+b)*2*PG_SIZE))
+#define NR_BANKS          4
 #endif
-#define FFS_PAGE_PTR(p,b) (uint16_t*)FFS_PAGE(p,b)
+
+//#define FFS_PAGE_PTR(p,b) (uint16_t*)ROMMAP_PAGE(p,b)
 
 #endif//__FLCONFIG_H__
--- fltools/fltools.cpp
+++ fltools/fltools.cpp
@@ -1,3 +1,4 @@
+#define NO_EXTERNAL
 #include <stdio.h>
 #include <string.h>
 #include <memory.h>
@@ -5,7 +6,6 @@
 #include "fltools.h"
 #include <sys/stat.h>
 #include "flconfig.h"
-#define NO_EXTERNAL
 #include "../modfile.h"
 
 
@@ -18,9 +18,17 @@ void initBin(FILE *bin, int offs)
   fprintf(bin, "# With 4MB flash this lead to 3584KB for images\n");
   fprintf(bin, "# Every page takes 8KB -> 448 ROM images!\n\n");
   fprintf(bin, "picotool=~/Projects/picotool/build/picotool\n\n");
-
-  fprintf(bin, "# Flash default load configuration\n");
-  fprintf(bin, "echo Flash load configuration\n");
+}
+void initConf(FILE *bin, int offs)
+{
+  fprintf(bin, "# Clear default module configuration\n");
+  fprintf(bin, "echo Erase module configuration\n");
+  fprintf(bin, "sudo $picotool load -v iconf.ini -t bin -o 0x%X\n", offs);
+}
+void initFat(FILE *bin, int offs)
+{
+  fprintf(bin, "# Flash default FAT configuration\n");
+  fprintf(bin, "echo Flash FAT\n");
   fprintf(bin, "sudo $picotool load -v iflash.ini -t bin -o 0x%X\n", offs);
 }
 
@@ -45,9 +53,54 @@ int isRamModule(char *m)
   ModuleFilePage MFP;
   fread(&MFH, sizeof(ModuleFileHeader), 1, fp);
   fread(&MFP, sizeof(ModuleFilePage), 1, fp);
-  printf("%s --> %s\n", MFP.header.Name, MFP.header.RAM ? "RAM" : "ROM");
+//  printf("%s --> %s\n", MFP.header.Name, MFP.header.RAM ? "RAM" : "ROM");
   fclose(fp);
   return MFP.header.RAM;
+}
+
+void dumpConfig(void)
+{
+  printf("Flash configuration\n========================================\n");
+  printf("Page size:       0x%04X (%d)\n", PG_SIZE, PG_SIZE);
+  printf("Port size:       0x%04X (%d)\n", PORT_SIZE, PORT_SIZE);
+  printf("Flash start:     0x%08X (%d)\n", FLASH_START, FLASH_START);
+  printf("Number of banks: %d\n", NR_BANKS);
+  printf("--------------------------------\n");
+  printf("XMEM page   %3d - %3d @ 0x%06X\n", XMEM_OFFS, XMEM_OFFS+XMEM_SIZE-1, XMEM_PAGE(0));
+  printf("CONF page   %3d - %3d @ 0x%06X\n", CONF_OFFS, CONF_OFFS+CONF_SIZE-1, CONF_PAGE(0));
+  printf("FAT page    %3d - %3d @ 0x%06X\n", FAT_OFFS, FAT_OFFS+FAT_SIZE-1, FAT_PAGE(0));
+  printf("ROMMAP page %3d - %3d @ 0x%06X\n", ROMMAP_OFFS, ROMMAP_OFFS+ROMMAP_SIZE-1, ROMMAP_PAGE(0,0));
+  printf("FS page     %3d - xxx @ 0x%06X\n", FS_OFFS, FS_START);
+  printf("========================================\n");
+  for(int p=0; p<XMEM_SIZE; p++) {
+    printf("0x%06X XMEM(%d)\n", XMEM_PAGE(p), p);
+  }
+  for(int p=0; p<CONF_SIZE; p++) {
+    printf("0x%06X CONF(%d)\n", CONF_PAGE(p), p);
+  }
+  for(int p=0; p<FAT_SIZE; p++) {
+    printf("0x%06X FAT(%d)\n", FAT_PAGE(p), p);
+  }
+  for(int p=0; p<16; p++) {
+    printf("0x%06X - 0x%06X ROMMAP(%d)\n", ROMMAP_PAGE(p,0), ROMMAP_PAGE(p,3), p);
+  }
+  printf("0x%06X FS\n", FS_START);
+}
+
+void dumpMap(char *m, int size)
+{
+  int rows=size/32;
+  int s = 0;
+  printf("\nFlash MAP (total %dMB)\n", size*PG_SIZE/(1024*1024));
+  printf("         +--------------------------------+\n");
+  for(int r=0; r<rows; r++) {
+    if( s && (s&0xFF) == 0 )
+      printf("         +--------------------------------+\n");
+
+    printf("0x%06X |%32.32s|\n", s*PG_SIZE, m+s);
+    s += 32;
+  }
+  printf("         +--------------------------------+\n");
 }
 
 int main(int argc, char *argv[])
@@ -61,14 +114,37 @@ int main(int argc, char *argv[])
   char *name;
   long fz;
   int mod = 0;
+  char *flMap = new char[1024];
+
+  memset(flMap,' ',1024);
 
   FILE *cfg = fopen("fltools.cfg","r");
   FILE *bin = fopen("iflash.sh", "w");
   FILE *ini = fopen("iflash.ini", "w");
+  FILE *cnf = fopen("iconf.ini", "w");
  
+  dumpConfig();
+
+  int pgOffs = (FLASH_START/PG_SIZE);
+  for(int pg=0; pg<pgOffs; pg++)
+    flMap[pg] = '.'; // Used for application
+
+  for(int pg=pgOffs+XMEM_OFFS; pg<(pgOffs+XMEM_OFFS+XMEM_SIZE); pg++)
+    flMap[pg] = 'X'; // Used for XMemory
+  for(int pg=pgOffs+CONF_OFFS; pg<(pgOffs+CONF_OFFS+CONF_SIZE); pg++)
+    flMap[pg] = 'C'; // Used for configuration
+  for(int pg=pgOffs+FAT_OFFS; pg<(pgOffs+FAT_OFFS+FAT_SIZE); pg++)
+    flMap[pg] = 'F'; // Used for FAT
+  for(int pg=pgOffs+ROMMAP_OFFS; pg<(pgOffs+ROMMAP_OFFS+ROMMAP_SIZE); pg++)
+    flMap[pg] = 'R'; // Used for ROMMAP
+
   initBin(bin, startOffs);
+  initConf(bin, CONF_PAGE(0));
+  initFat(bin, FAT_PAGE(0));
+
   // Point to start of FS (after FAT and FFS)
-  x += FAT_SIZE + FFS_SIZE;
+  // x += FAT_SIZE + ROMMAP_SIZE;
+  x = 0;
   // Read file info from config file
 	while(fgets(buf, 1024, cfg)) {
     if( buf[0] == '#' )
@@ -78,7 +154,7 @@ int main(int argc, char *argv[])
     p = &buf[strlen(buf)-1];
     if( *p == 0x0A )
       *p = 0;
-    printf("Check <%s>\n", buf);
+    //printf("Check <%s>\n", buf);
     np = strchr(buf,' ');
     if( np )  {
       *np = 0;
@@ -87,18 +163,22 @@ int main(int argc, char *argv[])
       while(*np == ' ')
         np++;
       name = np;
-      printf("Found name! <%s>\n", np);
+      //printf("Found name! <%s>\n", np);
     } else {
       name = 0;
 //      printf("No space found ...\n");
     }
     fz = GetFileSize(buf);
     if( fz == -1 ) {
-      printf("Warning: File <%s> not found ...\n",  buf);
+      char *n=&buf[strlen(buf)-1];
+      while( n>buf && *(n-1) != '/')
+        n--;
+      printf("Warning: File <%s> not found ...\n",  n);
       continue;
     }
     // Get offset to next free flash page
-    fl.offs = startOffs + x * PG_SIZE;
+    //fl.offs = startOffs + x * PG_SIZE;
+    fl.offs = FS_PAGE(x) ;
     mod++;
     saveBin(bin, mod, fl.offs, buf, fz);
     while(*p != '.')
@@ -122,6 +202,7 @@ int main(int argc, char *argv[])
     fwrite(&fl, sizeof(FL_Head_t), 1, ini);
     // Count and skip number of 4k pages
     while(fz>0) {
+      flMap[pgOffs+FS_OFFS+x] = 'M'; // Used for ROMMAP
       x++;
       fz -= PG_SIZE;
     }
@@ -129,7 +210,15 @@ int main(int argc, char *argv[])
   // Write the last (empty) FAT entry
   memset(&fl, 0, sizeof(FL_Head_t));
   fwrite(&fl, sizeof(FL_Head_t), 1, ini);
- 
+  uint8_t *conf = new uint8_t[CONF_SIZE*PG_SIZE];
+  memset(conf, 0, CONF_SIZE*PG_SIZE);
+  fwrite(conf, sizeof(FL_Head_t), 1, cnf);
+
+  dumpMap(flMap, 1024);
+
+  delete[] flMap;
+
+  fclose(cnf);
   fclose(ini);
   fclose(bin);
   fclose(cfg);
